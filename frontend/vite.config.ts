0c02a50f45bc04bad112ca4d1440e1780d8aa52c
Troy <iranzithierry12@gmail.com>
2026-06-10 17:47:07 +0200

    fix frontend bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(() => {
 	return {
 		plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+		base: "/"
 	};
 });
